890f0a8afc109703f7ef293adcb5b83890491064
name <Romanzaitsev25@yandex.by>
2022-05-21 21:43:31 +0300

    Refactoring coda

--- main_bot.py
+++ main_bot.py
@@ -1,30 +1,31 @@
 import datetime
 
 import requests
+from config import TOKEN, open_weather_token
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 
-from config import TOKEN, open_weather_token
 
 bot = Bot(TOKEN)
 dp = Dispatcher(bot)
 
 
 @dp.message_handler(commands=["Start"])
-async def start_command(message: types.Message):
+async def start_command(message):
     await message.reply(
         "Привет! Напиши мне название города и я пришлю сводку погоды!")
 
 
 @dp.message_handler(commands=["help"])
-async def help_command(message: types.Message):
+async def help_command(message):
     await message.reply(
         "Напиши мне, и я отправлю текс в ответ!")
 
 
 @dp.message_handler()
-async def get_weather(message: types.Message):
+async def get_weather(message):
+
     code_to_smile = {
         "Clear": "Ясно \U00002600",
         "Clouds": "Облачно \U00002601",
@@ -37,40 +38,40 @@ async def get_weather(message: types.Message):
 
     try:
         r = requests.get(
-            f"http://api.openweathermap.org/data/2.5/weather?q={message.text}&appid={open_weather_token}&units=metric"
+            f'http://api.openweathermap.org/data/2.5/weather?q={message.text}'
+            f'&appid={open_weather_token}&units=metric'
         )
         data = r.json()
 
-        city = data["name"]
-        cur_weather = data["main"]["temp"]
-
-        weather_description = data["weather"][0]["main"]
-        if weather_description in code_to_smile:
-            wd = code_to_smile[weather_description]
+        city = data['name']
+        cur_weather = data['main']['temp']
+        weather_descriptions = data['weather'][0]['main']
+        if weather_descriptions in code_to_smile:
+            wd = code_to_smile[weather_descriptions]
         else:
-            wd = "Посмотри в окно, не пойму что там за погода!"
+            wd = 'Посмотри в окно, не пойму что за погода на улице.'
+        humidity = data['main']['humidity']
+        pressure = data['main']['pressure']
+        wind = data['wind']['speed']
+        sunrise_timestamp =datetime.datetime.\
+            fromtimestamp(data['sys']['sunrise'])
+        sunset_timestamp = datetime.datetime. \
+            fromtimestamp(data['sys']['sunset'])
+        length_of_the_day = datetime.datetime.fromtimestamp\
+            (data['sys']['sunset']) - datetime.datetime.fromtimestamp\
+            (data['sys']['sunrise'])
 
-        humidity = data["main"]["humidity"]
-        pressure = data["main"]["pressure"]
-        wind = data["wind"]["speed"]
-        sunrise_timestamp = datetime.datetime.fromtimestamp(
-            data["sys"]["sunrise"])
-        sunset_timestamp = datetime.datetime.fromtimestamp(
-            data["sys"]["sunset"])
-        length_of_the_day = datetime.datetime.fromtimestamp(
-            data["sys"]["sunset"]) - datetime.datetime.fromtimestamp(
-            data["sys"]["sunrise"])
-
-        await message.reply(
-            f"***{datetime.datetime.now().strftime('%Y-%m-%d %H:%M')}***\n"
-            f"Погода в городе: {city}\nТемпература: {cur_weather}C° {wd}\n"
-            f"Влажность: {humidity}%\nДавление: {pressure} мм.рт.ст\nВетер: {wind} м/с\n"
-            f"Восход солнца: {sunrise_timestamp}\nЗакат солнца: {sunset_timestamp}\nПродолжительность дня: {length_of_the_day}\n"
-            f"***Хорошего дня!***"
-            )
+        await message.reply(f"***{datetime.datetime.now().strftime('%Y-%m-%d %H:%M')}***\n"
+              f"Погода в городе: {city}\nТемпература: {cur_weather}C°{wd}\n"
+              f"Влажность: {humidity}%\nДавление: {pressure} мм.рт.ст\n"
+              f"Ветер: {wind} м/с\nВосход солнца: {sunrise_timestamp}\n"
+              f"Закат солнца: {sunset_timestamp}\n"
+              f"Продолжительность дня: {length_of_the_day}\n"
+              f"***Хорошего дня!***"
+              )
 
     except:
-        await message.reply("\U00002620 Проверьте название города \U00002620")
+        await message.reply("\U00002620 Проверьте название города\U00002620")
 
 
 if __name__ == '__main__':
